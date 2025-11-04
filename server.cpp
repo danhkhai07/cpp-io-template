f68c317d41b5e08f86e594bb57c8847c884bfbd9
danhkhai07 <aliveomg@gmail.com>
2025-11-04 10:47:47 +0000

    fix

--- server.cpp
+++ server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -13,20 +14,34 @@ int main(){
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+	    perror("Bind failed");
+	    return 1;
+}
 
-    listen(serverSocket, 5);
+	if (listen(serverSocket, 5) < 0) {
+    perror("Listen failed");
+    return 1;
+}
 
     int clientSocket = accept(serverSocket, nullptr, nullptr);
     std::cout << "Client connected!\n";
 
-    char buffer[1024];
-    while (buffer != "EOC"){
-        recv(clientSocket, buffer, strlen(buffer), 0);
-        //std::cout << "Message from client: " << buffer << std::endl;
-        if (buffer[0] != '\0') {
-            std::cout << buffer << std::endl;
+    std::string message = "";
+    std::string buffer;
+    buffer.resize(1024);
+    for (;;){
+        int bytes = recv(clientSocket, buffer.data(), buffer.size(), 0);
+	if (bytes <= 0) break;
+	buffer.resize(bytes);
+	//std::cout << "Buffer: " << buffer << "._." << std::endl;
+	message += buffer;
+	//std::cout << "Message: " << message << ' ' << message[message.size()-2] << std::endl;
+        if (message.size() >= 1 && message[message.size()-2] == '$') {
+            std::cout << "Message from client: " << message << std::endl;
+	    message = "";
         }
+	buffer.resize(1024);
     }
 
     close(serverSocket);
